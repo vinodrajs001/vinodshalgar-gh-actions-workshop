943effe5723e91db401f51cd7ec9e6f4671455eb
vinodrajs001 <71584020+vinodrajs001@users.noreply.github.com>
2024-01-14 13:48:19 +0000

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  test: {
+    environment: "jsdom",
+    coverage: {
+      reporter: ["text", "json", "json-summary"],
+      lines: 80,
+      branches: 80,
+      functions: 80,
+      statements: 80
+    },
+    setupFiles: ["./test/setup.ts"],
+  },
+});
